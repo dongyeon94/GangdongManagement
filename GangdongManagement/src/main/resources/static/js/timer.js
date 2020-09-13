85ff45a8f8c94baf4f18b2a300a132533b3d3957
dongyeon94 <37605781+dongyeon94@users.noreply.github.com>
2020-09-13 16:11:08 +0700

    Update timer.js

--- GangdongManagement/src/main/resources/static/js/timer.js
+++ GangdongManagement/src/main/resources/static/js/timer.js
@@ -1,13 +1,11 @@
-
 checkTime();
 function checkTime()
 {
-	var d = new Date(); 
-	var currentDate = d.getFullYear() + "년 " + ( d.getMonth() + 1 ) + "월 " + d.getDate() + "일"; 
-	var currentTime = d.getHours() + "시 " + d.getMinutes() + "분 " + d.getSeconds() + "초"; 
-	var result = document.getElementById("today-time"); 
-	result.innerHTML =  currentDate +  currentTime;			 
-}		
+	var d = new Date();
+	var currentDate = d.getFullYear() + "년 " + (d.getMonth() + 1) + "월 " + d.getDate() + "일";
+	var currentTime = d.getHours() + "시 " + d.getMinutes() + "분 " + d.getSeconds() + "초";
+	var result = document.getElementById("today-time");
+	result.innerHTML =  currentDate +  currentTime;
+}
 setInterval("checkTime()",1000);
-document.getElementById("currentMonth").innerHTML = ( new Date().getMonth() + 1 ) + "월 ";
-
+document.getElementById("currentMonth").innerHTML = (new Date().getMonth() + 1) + "월 ";
