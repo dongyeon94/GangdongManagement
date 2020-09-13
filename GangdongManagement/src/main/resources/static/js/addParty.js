e5de57bd6cd08e974a5b3d992cbd94f6629a50eb
dongyeon94 <37605781+dongyeon94@users.noreply.github.com>
2020-09-13 16:10:21 +0700

    Update addParty.js

--- GangdongManagement/src/main/resources/static/js/addParty.js
+++ GangdongManagement/src/main/resources/static/js/addParty.js
@@ -1,81 +1,89 @@
+var userList = [];
+var userNicknameList =[];
+var partyDate = "";
+var partyTimes = "";
+var addPartyVal;
 
-
-function addPartyConfirm(){	
-	var addPartyVal = jQuery(".userchecked").serialize().split('&');
-	var userList = [];
-	var partyDate = "";
-	var partyTimes = "";
-	
-	for(var i in addPartyVal){
-		userList.push( addPartyVal[i].split("=")[1] );
-	}
-					
-	var addPartyDateVal = jQuery(".ADDDateTimes").serialize().split('&');
-	for(var i in addPartyDateVal){
-		var  splitLi = addPartyDateVal[i].split('=');
-		if(splitLi[0]==='datetimes'){
-			if(splitLi.length>1){
-				partyDate =splitLi[1];
-				console.log(partyDate);
-			}	
-			else{
-				alert("please write date");
-			}
-		}
-		if(splitLi[0]==='times'){
-			if(splitLi.length>1){
-				partyTimes = splitLi[1];
-				console.log(partyTimes);
-			}
-			else{
-				alert("please write times");				
-			}
-		}					
-	}
-	
+function addPartyConfirm()
+{
 	var partyData = {'datetimes' :partyDate,
 			'times' : partyTimes,
 			'user' : userList };
-	console.log(userList);
-	console.log(userList.length);
+
 	if(partyDate.length > 1 &&  userList.length >1)
 	{
-		$.ajax({
+		$.ajax
+        ({
 			type:		"POST",
 			url :		"http://localhost:8080/addParty",
 			data:		JSON.stringify(partyData),
 			contentType: "application/json; charset=UTF-8",
 			dataType:	"json",
-			success:function(){consle.log("success") }
-				
-			});			
-		return true;	
+			success:function(){ consle.log("success") }
+		});
+		return true;
 	}
 	else
 	{
 		return false;
-	}	
+	}
 }
 
-function addPartyAjax(){
-	if(confirm("정말 등록하시겠습니까 ?") == true){
-        
-		var confirmFlag = addPartyConfirm();
-        if (confirmFlag==true)
+function addPartyAjax()
+{
+	addPartyVal = jQuery(".userchecked").serialize().split('&');
+
+	for(var i in addPartyVal)
+    {
+		userList.push( addPartyVal[i].split("=")[1] );
+	}
+
+	var addPartyDateVal = jQuery(".ADDDateTimes").serialize().split('&');
+	for(var i in addPartyDateVal)
+    {
+		var  splitLi = addPartyDateVal[i].split('=');
+
+        if (splitLi.length >= 2)
         {
-        	alert("등록되었습니다");
-        	location.reload(true);
+            if(splitLi[0] === 'datetimes')
+            {
+                partyDate = splitLi[1];
+            }
+            if(splitLi[0] === 'times')
+            {
+                partyTimes = splitLi[1];
+            }
         }
         else
         {
-        	alert("등록에 실패하였습니다.");
-            location.reload(true);
-            return true;	
-        }      
+            alert("등록에러, 입력을 확인해 주세요");
+        }
+	}
+
+	var bResult = false;
+	var msg = "";
+	var confirmMessage = confirm("정말 등록하시겠습니까 ? \n"+ "날짜 : " + partyDate + " \n"+partyTimes+" 차 벙 \n" );
+
+	if(true == confirmMessage)
+	{
+    	var confirmFlag = addPartyConfirm();
+
+        if (true == confirmFlag)
+        {
+        	msg = "등록 되었습니다";
+        }
+        else
+        {
+        	msg = "등록 실패";
+            bResult = true;
+        }
     }
-    else{
-    	alert("취소 되었습니다");
-    	location.reload(true);
-        return false;
-    }						
+    else
+    {
+    	msg = "취소 되었습니디";
+    }
+
+    alert(msg);
+    location.reload(true);
+    return bResult;
 }
