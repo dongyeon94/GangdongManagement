ede731476d7712aa3ccef076d8e23058e0998b42
dongyeon94 <37605781+dongyeon94@users.noreply.github.com>
2020-09-13 16:10:50 +0700

    Update deleteMember.js

--- GangdongManagement/src/main/resources/static/js/deleteMember.js
+++ GangdongManagement/src/main/resources/static/js/deleteMember.js
@@ -1,36 +1,36 @@
-
-function deleteMemberConfirm(){
+var msg = "";
+function deleteMemberConfirm()
+{
 	var deleteMemberVal = jQuery(".deleteUser").serialize().split('&');
 	var userList = [];
-	
-	for(var i in deleteMemberVal){
+
+	for(var i in deleteMemberVal)
+    {
 		userList.push( deleteMemberVal[i].split("=")[1] );
 	}
-	console.log(userList);
+
 	var MemberData = {'member' :userList};
-		$.ajax({
+
+    $.ajax
+    ({
 		type:		"POST",
 		url :		"http://localhost:8080/deleteUser",
 		data:		JSON.stringify(MemberData),
 		contentType: "application/json; charset=UTF-8",
 		dataType:	"json",
-		success:function(){consle.log("success") }					
-	});		
+		success:function(){ consle.log("success") }
+	});
 }
 
-function deleteMemberAjax(){
-		
-	if(confirm("정말 삭제하시겠습니까 ?\n   ") == true){
-        alert("삭제되었습니다");
+function deleteMemberAjax()
+{
+	if(confirm("정말 삭제하시겠습니까? \n") == true){
+        msg = "삭제되었습니다";
         deleteMemberConfirm();
-        location.reload(true);
-        return true;
     }
     else{
-    	alert("취소 되었습니다");
-    	location.reload(true);
-        return false;
+        msg = "취소 되었습니다";
     }
-
+    alert(msg);
+    location.reload(true);
 }
-
